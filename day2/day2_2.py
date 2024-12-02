fbc3cb166fb01b88464a5ec97ef95cbfb44baf88
vmaccio <v.maccio@alumnos.upm.es>
2024-12-02 23:51:35 +0100

    dia 2 - 2

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -1,27 +1,62 @@
-file1 = open("/home/valentin/Documentos/AdventOfCode24/day1/inputD1.txt", "r")
+file1 = open("/home/valentin/Documentos/AdventOfCode24/day2/inputD2.txt", "r")
 
-list1 = []
-list2 = []
+suma = 0
+
+def legalOrNot(entrada):
+    #print(entrada)
+    legal = 0;
+    index = 2
+    previo = int(entrada[0])
+    siguiente = int(entrada[1])
+    if (previo > siguiente and abs(previo-siguiente) <= 3):
+        while index < len(entrada):
+            previo = siguiente
+            siguiente = int(entrada[index])
+            if(previo > siguiente and abs(previo-siguiente) <= 3):
+                if(index + 1 == len(entrada)):
+                    legal += 1
+            else:
+                return index
+            index += 1
+    elif (previo < siguiente and abs(previo-siguiente) <= 3):
+        while index < len(entrada):
+            previo = siguiente
+            siguiente = int(entrada[index])
+            if(previo < siguiente and abs(previo-siguiente) <= 3):
+                if(index + 1 == len(entrada)):
+                    legal += 1
+            else:
+                return index
+            index += 1
+    else:
+        return index
+    if(legal > 0):
+        return -1
+    else:
+        return 0
+
 while True:
     line = file1.readline()
     if not line: break
     separate = line.split(" ")
-    list1.append(int(separate.pop(0)))
-    temp = separate.pop(2)
-    temp.rstrip()
-    list2.append(int(temp))
 
-#ya estan las listas separadas, falta operar
-index1 = 0
-index2 = 0
-suma = 0
-while index1 < len(list1):
-    while index2 < len(list2):
-        if(list1[index1] == list2[index2]):
-            suma = suma + list1[index1]
-        index2 += 1
-    index1 += 1
-    index2 = 0
+    temp = legalOrNot(separate)
+    if (temp == -1):
+        suma += 1
+    else:
+        copy1 = separate.copy()
+        copy2 = separate.copy()
+        copy3 = separate.copy()
+        copy1.pop(temp)
+        #print(copy1)
+        copy2.pop(temp - 1)
+        #print(copy2)
+        copy3.pop(temp - 2)
+        #print(copy3)
+        if (legalOrNot(copy1) == -1 or legalOrNot(copy2) == -1 or legalOrNot(copy3) == -1):
+            suma += 1
 
-print(suma)
+
+print(suma) #802 too high, 468 too low. 476 just perfect
+
 file1.close
